501890ffab8d240f683de7c4e632baadc12d5d16
Jingjing Bao <JBao6@slb.com>
2020-08-10 08:18:18 +0800

    clean code

--- include/processor.h
+++ include/processor.h
@@ -3,9 +3,8 @@
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
-
-  // TODO: Declare any necessary private members
+  float Utilization();  
+  
  private:
 };
 
